f7cd753309adf0a38f88f6d166e27ee9576dc643
adrianvilla1504 <adriancvilla@gmail.com>
2022-10-27 20:15:41 -0500

    feat: init react, rtk config

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { taskSlice } from '../features/task/taskSlice'
+
+const store =  configureStore({
+
+})
+
+export default store;
---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const [name, setName] = useState(0);
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState: [],
+  reducers: {
+
+  }
+})
